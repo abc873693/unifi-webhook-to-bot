8497fd76bfd19f3da5d55d7596092fff1f0f89fd
Rainvistor <abc873693@gmail.com>
2025-10-19 14:49:34 +0800

    feat: unifi support thumbnail version

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ from fastapi import FastAPI, Request
 from telegram import Bot
 import uvicorn
 import asyncio
-import jwt
+import base64
 
 app = FastAPI()
 
 WEB_HOOK_ID = os.environ.get("WEB_HOOK_ID")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
-UNIFI_TOKEN = os.environ.get("UNIFI_TOKEN")
-
-cookies = {
-    "TOKEN": UNIFI_TOKEN
-}
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
@@ -27,19 +22,12 @@ async def receive_webhook(webhook_id: str, request: Request):
     if webhook_id != WEB_HOOK_ID:
         return {"status": "error", "message": "Invalid webhook ID"}
     
-    asyncio.create_task(delayed_notify(data))
+    asyncio.create_task(notify(data))
 
     return {"status": "success", "webhook_id": webhook_id}
 
 
-async def delayed_notify(data):
-    await asyncio.sleep(60)
-
-    event_id = (
-        data.get("alarm", {})
-            .get("triggers", [{}])[0]
-            .get("eventId")
-    )
+async def notify(data):
 
     event_type = (
         data.get("alarm", {})
@@ -58,52 +46,27 @@ async def delayed_notify(data):
             .get("name")
     )
 
-    image_url = f'https://192.168.0.1/proxy/protect/api/thumbnails/e-{event_id}'
-
-    response = requests.get(
-        image_url, 
-        cookies={
-            "TOKEN": UNIFI_TOKEN
-        }, 
-        verify=False
+    thumbnail_base64 = (
+        data.get("alarm", {})
+            .get("thumbnail")
     )
-    if response.status_code == 200:
+
+    if thumbnail_base64:
         dt = datetime.fromtimestamp(timestamp / 1000)
-        filename = f"./output/{dt}.png"
-        with open(filename, "wb") as f:
-            f.write(response.content)
-
-        with open(filename, "rb") as photo_file:
-            await bot.send_photo(
-                chat_id=CHAT_ID,
-                photo=photo_file,
-                caption= f"*{event_name}* for _{event_type}_",
-                parse_mode="Markdown"
-            )
-    else:
-        print("下載圖片失敗", response.status_code)
-    
-
-    try:
-        payload = jwt.decode(UNIFI_TOKEN, options={"verify_signature": False})
-        exp = payload.get("exp")
-        now = int(datetime.now().timestamp())
-        if exp:
-            if now > exp:
-                await bot.sendMessage(
-                    chat_id=CHAT_ID,
-                    text= "UNIFI_TOKEN 已過期",
-                    parse_mode="Markdown"
-                )
-                print("UNIFI_TOKEN 已過期")
-            elif exp - now <= 2 * 24 * 60 * 60:
-                await bot.sendMessage(
-                    chat_id=CHAT_ID,
-                    text= f"UNIFI_TOKEN 兩天內即將過期，時間 {datetime.fromtimestamp(exp)}",
-                    parse_mode="Markdown"
-                )
-    except Exception as e:
-        print("JWT 解析失敗:", e)
+        thumbnail_filename = f"./output/{dt}.png"
+        
+        if thumbnail_base64.startswith('data:image'):
+            thumbnail_base64 = thumbnail_base64.split(',')[1]
+        
+        with open(thumbnail_filename, "wb") as f:
+            f.write(base64.b64decode(thumbnail_base64))
+        
+        await bot.send_photo(
+            chat_id=CHAT_ID,
+            photo=thumbnail_filename,
+            caption= f"*{event_name}* for _{event_type}_",
+            parse_mode="Markdown"
+        )
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
